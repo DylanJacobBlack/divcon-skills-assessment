d2c46b9cf5d2600c4e4c5fc3192f3bf75ce95640
Dylan Black <dylan.jacob.black@gmail.com>
2022-09-19 08:17:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(
+          data.sort((a, b)=>(b.id - a.id)).slice(0, 21).map((post) => (
+            <tr>
+              <td>{post.id}</td>
+              <td>{post.userId}</td>
+              <td>{post.title}</td>
+              <td>{post.body}</td>
+            </tr>
+          ))
+        );
+      });
+  }, [setPosts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-5">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">User Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Body</th>
+          </tr>
+        </thead>
+        <tbody>{posts}</tbody>
+      </table>
     </div>
   );
 }
